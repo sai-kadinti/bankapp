c4feb6d6c7d43824995ad1847a5d8f9dd0a9d93c
sai-kadinti <85397181+sai-kadinti@users.noreply.github.com>
2025-07-26 01:08:28 +0530

    Create HomeControllerTests.java

---
+++ src/main/java/com/example/banking/HomeControllerTests.java
@@ -0,0 +1,12 @@
+package com.example.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeControllerTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
